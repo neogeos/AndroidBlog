eb2f058b26e309b18c6cce3e46a7b5618f901cb2
JW <JW@DESKTOP-SOGB1LT>
2018-11-02 13:22:10 -0400

    Ajustes detalles de usuarios obtener uid

--- app/src/main/java/com/example/jw/androidblog/PostActivity.java
+++ app/src/main/java/com/example/jw/androidblog/PostActivity.java
@@ -3,6 +3,7 @@ package com.example.jw.androidblog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,10 +13,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -31,14 +38,21 @@ public class PostActivity extends AppCompatActivity {
     private StorageReference mStorage;
     private DatabaseReference mDatabase;
     private ProgressDialog mProgress;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+    private DatabaseReference mDatabaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
 
         mStorage = FirebaseStorage.getInstance().getReference();
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
+        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
 
         mPostTitle = (EditText) findViewById(R.id.tittleField);
         mPostDesc = (EditText) findViewById(R.id.descField);
@@ -79,19 +93,42 @@ public class PostActivity extends AppCompatActivity {
 
                     filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                         @Override
-                        public void onSuccess(Uri uri) {
+                        public void onSuccess(final Uri uri) {
                             Log.d("URL", uri.toString());
                             // This is the complete uri, you can store it to realtime database
 
 
                     //Uri downloadUri = taskSnapshot.getUploadSessionUri();
                     //Task<Uri> downloadUri = taskSnapshot.getStorage().getDownloadUrl();
-                    DatabaseReference newPost = mDatabase.push();
-                    newPost.child("title").setValue(title_val);
-                    newPost.child("desc").setValue(desc_val);
-                    newPost.child("image").setValue(uri.toString());
+                    final DatabaseReference newPost = mDatabase.push();
+
+
+                    mDatabaseUser.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            newPost.child("title").setValue(title_val);
+                            newPost.child("desc").setValue(desc_val);
+                            newPost.child("image").setValue(uri.toString());
+                            newPost.child("uid").setValue(mCurrentUser.getUid());
+                            newPost.child("username").setValue(dataSnapshot.child("name").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        startActivity(new Intent(PostActivity.this, MainActivity.class));
+                                    }
+                                }
+                            });
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
                     mProgress.dismiss();
-                    startActivity(new Intent(PostActivity.this, MainActivity.class));
 
                         }
                     });
